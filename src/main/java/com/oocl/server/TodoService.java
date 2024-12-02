2372691c908e5e122515d3435b033d3465afe96b
SHENZA <shenza@oocl.com>
2024-12-02 15:40:36 +0800

    [todo]feat,add todoService

---
+++ src/main/java/com/oocl/server/TodoService.java
@@ -0,0 +1,35 @@
+package com.oocl.server;
+
+import com.oocl.model.Todo;
+import com.oocl.repository.TodoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    private final TodoRepository todoRepository;
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> findAll(){
+        return todoRepository.findAll();
+    }
+
+    public Todo save(Todo todo){
+        return todoRepository.save(todo);
+    }
+    public Todo update(Todo todo){
+        return todoRepository.save(todo);
+    }
+    public void delete(Integer id){
+        todoRepository.deleteById(id);
+    }
+
+    public Todo getById(Integer id){
+        return todoRepository.findById(id).orElse(null);
+    }
+
+}
