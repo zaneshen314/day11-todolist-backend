70a7c7c8503d332eae8c1f88d7198f6d9a308bcd
SHENZA <shenza@oocl.com>
2024-12-02 12:42:22 +0800

    [todo]chore,init project

---
+++ src/main/java/com/oocl/TodoListApplication.java
@@ -0,0 +1,14 @@
+package com.oocl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication
+public class TodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListApplication.class, args);
+    }
+
+}
