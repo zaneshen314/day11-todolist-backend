bbb0bd164082be52a27f3c99655450fa66b43f0b
SHENZA <shenza@oocl.com>
2024-12-02 22:18:39 +0800

    [todo]chore,upload build gradle

--- src/main/java/com/oocl/TodoListApplication.java
+++ src/main/java/com/oocl/TodoListApplication.java
@@ -2,8 +2,6 @@ package com.oocl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 @SpringBootApplication
 public class TodoListApplication {
 
